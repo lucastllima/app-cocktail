8c84b0ce4e9aa8e62c268ea9f045e621b4843f90
lucastllima <lucasteixeira-lima@hotmail.com>
2023-08-10 17:55:27 -0300

    Initial commit

---
+++ components/tests/mock.test.ts
@@ -0,0 +1,7 @@
+import { expect, test } from "vitest";
+
+describe("generic test", () => {
+  test("adds 1 + 2 to equal 3", () => {
+    expect(1 + 2).toBe(3);
+  });
+});
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1 class="text-3xl">Drink Discover</h1>
+</template>
+
+<script lang="ts" setup></script>
