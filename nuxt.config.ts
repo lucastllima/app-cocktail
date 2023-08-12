5a7eabb47aaba61887d8e0bcce109c86eb16fdbc
lucastllima <lucasteixeira-lima@hotmail.com>
2023-08-12 16:20:11 -0300

    chore(nuxt.config): define runtime baseUrl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  runtimeConfig: {
+    public: {
+      baseUrl: "https://www.thecocktaildb.com/api/json/v1/1"
+    }
+  }
 })
