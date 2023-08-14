8b193d913671cd50d149381b37ced6d2a52842fb
lucastllima <lucasteixeira-lima@hotmail.com>
2023-08-14 02:54:03 -0300

    feat: Add error page

---
+++ error.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex items-center justify-center h-screen">
+    <div class="text-center">
+      <h1 class="text-6xl font-bold mb-4 text-red-500">404</h1>
+      <p class="text-xl font-semibold">As bebidas não estão nessa página.</p>
+      <BaseButton class="mt-4" @click="$router.push('/')">
+        Ir para página de bebidas
+      </BaseButton>
+    </div>
+  </div>
+</template>
